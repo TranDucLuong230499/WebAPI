224eb448ea5bc4da4c8ec4150ed957d8561e5ffc
vincent.9xz@gmail.com <vincent.9xz@gmail.com>
2020-10-27 22:41:17 +0700

    Add project files.

---
+++ Models/InventoryContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class InventoryContext : DbContext
+    {
+        protected InventoryContext()
+        {
+        }
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+        
+        protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-7HGH6SF;Database=Inventory;Trusted_Connection=True;");
+            }
+        }
+    }
+}
